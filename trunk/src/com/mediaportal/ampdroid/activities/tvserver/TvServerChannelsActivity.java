eb08ad0f13b2b2e026d3a6815675ff7b1ca839d4
bgmeiner <bgmeiner@9844d840-c6a8-26a1-2f71-7252c05e7768>
2011-01-12 23:09:13 +0000

    - improved background loading of "Manual Control" section - proper implemented background loading for channels section - implemented custom layout for recordings listview - added preference for "Show all channels"

--- trunk/src/com/mediaportal/ampdroid/activities/tvserver/TvServerChannelsActivity.java
+++ trunk/src/com/mediaportal/ampdroid/activities/tvserver/TvServerChannelsActivity.java
@@ -1,7 +1,10 @@
 package com.mediaportal.ampdroid.activities.tvserver;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,45 +13,89 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 
+import com.mediaportal.ampdroid.R;
 import com.mediaportal.ampdroid.activities.BaseActivity;
 import com.mediaportal.ampdroid.api.DataHandler;
 import com.mediaportal.ampdroid.data.TvChannel;
 import com.mediaportal.ampdroid.data.TvChannelGroup;
-import com.mediaportal.ampdroid.R;
+
 public class TvServerChannelsActivity extends BaseActivity {
    private DataHandler mService;
    private ListView mListView;
    private Spinner mGroupsSpinner;
-   
+
+   ArrayAdapter<TvChannel> mChannelItems;
+   ArrayAdapter<TvChannelGroup> mGroupsItems;
+   UpdateGroupsTask mGroupsUpdater;
+   UpdateChannelsTask mChannelUpdater;
+   ProgressDialog mLoadingDialog;
+
+   boolean mShowAllGroup = false;
+
+   private class UpdateGroupsTask extends AsyncTask<Integer, Integer, List<TvChannelGroup>> {
+      @Override
+      protected List<TvChannelGroup> doInBackground(Integer... _group) {
+         ArrayList<TvChannelGroup> groups = mService.getTvChannelGroups();
+         return groups;
+      }
+
+      @Override
+      protected void onPostExecute(List<TvChannelGroup> _result) {
+         mGroupsItems.clear();
+         if (_result != null) {
+            for (TvChannelGroup g : _result) {
+               if (mShowAllGroup || g.getIdGroup() != 1) {
+                  mGroupsItems.add(g);
+               }
+            }
+         }
+      }
+   }
+
+   private class UpdateChannelsTask extends AsyncTask<TvChannelGroup, Integer, List<TvChannel>> {
+      @Override
+      protected List<TvChannel> doInBackground(TvChannelGroup... _group) {
+         List<TvChannel> channels = mService.getTvChannelsForGroup(_group[0].getIdGroup()); // get
+         return channels;
+      }
+
+      @Override
+      protected void onPostExecute(List<TvChannel> _result) {
+         if (_result != null) {
+            mChannelItems.clear();
+
+            for (TvChannel c : _result) {
+               mChannelItems.add(c);
+            }
+         }
+         mLoadingDialog.cancel();
+      }
+   }
+
    @Override
    public void onCreate(Bundle _savedInstanceState) {
       super.onCreate(_savedInstanceState);
       setHome(false);
       setTitle(R.string.title_tvserver_channels);
-      
+
       setContentView(R.layout.tvserverchannelsactivity);
       mListView = (ListView) findViewById(R.id.ListViewChannels);
-      mGroupsSpinner = (Spinner)findViewById(R.id.SpinnerGroups);
+      mChannelItems = new ArrayAdapter<TvChannel>(this, android.R.layout.simple_list_item_1);
+      mListView.setAdapter(mChannelItems);
+      
+      mGroupsSpinner = (Spinner) findViewById(R.id.SpinnerGroups);
+      mGroupsItems = new ArrayAdapter<TvChannelGroup>(this, android.R.layout.simple_spinner_item);
+      mGroupsItems.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      mGroupsSpinner.setAdapter(mGroupsItems);
+
       mService = DataHandler.getCurrentRemoteInstance();
-      
-      if (mService.isTvServiceActive()) {
-         List<TvChannelGroup> groups = mService.getTvChannelGroups();
-         if (groups != null) {
-            ArrayAdapter<TvChannelGroup> adapter = new ArrayAdapter<TvChannelGroup>(this,
-                  android.R.layout.simple_spinner_item, groups);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-            mGroupsSpinner.setAdapter(adapter);
-         }
-      }
-      
       mGroupsSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
          @Override
          public void onItemSelected(AdapterView<?> _adapter, View _view, int _position, long _id) {
             TvChannelGroup group = (TvChannelGroup) mGroupsSpinner.getItemAtPosition(_position);
-            
-            fillChannelListView(group);
+
+            refreshChannelList(group);
          }
 
          @Override
@@ -56,22 +103,26 @@ public class TvServerChannelsActivity extends BaseActivity {
             // TODO Auto-generated method stub
          }
       });
-   }
-   
-
-   private void fillChannelListView(TvChannelGroup _group) {
-     List<TvChannel> channels = mService.getTvChannelsForGroup(_group.getIdGroup());
-     
-     if (channels != null) {
-        ArrayAdapter<TvChannel> adapter = new ArrayAdapter<TvChannel>(this,
-              android.R.layout.simple_list_item_1);
-        
-        for(TvChannel c : channels){
-           adapter.add(c);
-        }
-
-        mListView.setAdapter(adapter);
-     }
       
+      refreshGroups();
+   }
+
+   private void refreshGroups() {
+      mLoadingDialog = ProgressDialog.show(TvServerChannelsActivity.this, " Loading Groups ",
+            " Loading. Please wait ... ", true);
+      mLoadingDialog.setCancelable(true);
+
+      mGroupsUpdater = new UpdateGroupsTask();
+      mGroupsUpdater.execute(0);
+   }
+
+   private void refreshChannelList(TvChannelGroup _group) {
+      mLoadingDialog.cancel();
+      mLoadingDialog = ProgressDialog.show(TvServerChannelsActivity.this, " Loading Channels ",
+            " Loading. Please wait ... ", true);
+      mLoadingDialog.setCancelable(true);
+
+      mChannelUpdater = new UpdateChannelsTask();
+      mChannelUpdater.execute(_group);
    }
 }
--- trunk/src/com/mediaportal/ampdroid/activities/tvserver/TvServerRecordingsActivity.java
+++ trunk/src/com/mediaportal/ampdroid/activities/tvserver/TvServerRecordingsActivity.java
@@ -9,10 +9,13 @@ import android.widget.ListView;
 import com.mediaportal.ampdroid.activities.BaseActivity;
 import com.mediaportal.ampdroid.api.DataHandler;
 import com.mediaportal.ampdroid.data.TvRecording;
+import com.mediaportal.ampdroid.lists.LazyLoadingAdapter;
+import com.mediaportal.ampdroid.lists.views.TvServerRecordingsThumbsView;
 import com.mediaportal.ampdroid.R;
 public class TvServerRecordingsActivity extends BaseActivity {
    private DataHandler mService;
    private ListView mListView;
+   private LazyLoadingAdapter mAdapter;
    
    @Override
    public void onCreate(Bundle _savedInstanceState) {
@@ -21,19 +24,16 @@ public class TvServerRecordingsActivity extends BaseActivity {
       super.onCreate(_savedInstanceState);
       setContentView(R.layout.tvserverrecordingsactivity);
       mListView = (ListView) findViewById(R.id.ListViewRecordings);
-      
+      mAdapter = new LazyLoadingAdapter(this, R.layout.listitem_recordings_thumbs);
       mService = DataHandler.getCurrentRemoteInstance();
       if (mService.isTvServiceActive()) {
          List<TvRecording> recordings = mService.getTvRecordings();
          if (recordings != null) {
-            ArrayAdapter<TvRecording> adapter = new ArrayAdapter<TvRecording>(this,
-                  android.R.layout.simple_list_item_1);
-            
             for(TvRecording r : recordings){
-               adapter.add(r);
+               mAdapter.AddItem(new TvServerRecordingsThumbsView(r));
             }
 
-            mListView.setAdapter(adapter);
+            mListView.setAdapter(mAdapter);
          }
       }
    }
